a53a8cbae1999de2aa21bb2443ebe9bdd7a5eaf9
Ahmad Anvari <ahmad.anv@gmail.com>
2024-05-16 10:28:51 +0330

    feat: implement a new navbar mechanism 🧭

---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<script>
+import {RouterLink} from "vue-router";
+
+import {mapGetters} from "vuex";
+
+export default {
+  name: "NavBar",
+  components: {RouterLink},
+  computed: {
+    ...mapGetters(["isLoggedIn"])
+  }
+}
+</script>
+
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">shirinibe.de</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li v-if="isLoggedIn">
+          <RouterLink to="/teams">Teams</RouterLink>
+        </li>
+        <li v-if="isLoggedIn">
+          <RouterLink to="/create-team">Create Team</RouterLink>
+        </li>
+        <li v-if="!isLoggedIn">
+          <RouterLink to="/login">Login</RouterLink>
+        </li>
+        <li v-if="isLoggedIn">
+          <RouterLink to="/login">Logout</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
