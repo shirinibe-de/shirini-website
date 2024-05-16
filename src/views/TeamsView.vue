5abe092c829c5047095155f46a8cb87e665438a0
Ahmad Anvari <ahmad.anv@gmail.com>
2024-05-16 10:32:36 +0330

    feat: implement teams list view 👀

---
+++ src/views/TeamsView.vue
@@ -0,0 +1,27 @@
+<script>
+import TeamListItem from "@/components/TeamListItem.vue";
+import api from "@/api/api"
+
+export default {
+  components: {TeamListItem},
+  data() {
+    return {
+      teams: []
+    }
+  },
+  async mounted() {
+    this.teams = await api.getTeams();
+  }
+}
+</script>
+
+<template>
+    <div class="row">
+      <TeamListItem v-for="team in teams" v-bind:name="team.name" v-bind:id="team.id"></TeamListItem>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
